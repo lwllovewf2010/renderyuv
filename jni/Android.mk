f66dfed866a569d03985e59d767f8fccab7fbf92
mamk <mamk@mamkdeMacBook-Pro.local>
2014-12-10 15:39:47 +0800

    fix compile problems

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,10 +2,36 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)  
 LOCAL_SRC_FILES:= showYUV.cpp  
 
-LOCAL_C_INCLUDES := include \
-	include/system/core/include/ \
-      
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include/frameworks/base/include \
+	$(LOCAL_PATH)/include/frameworks/native/include \
+	$(LOCAL_PATH)/include/system/core/include \
+	$(LOCAL_PATH)/include/libnativehelper/include \
+	$(LOCAL_PATH)/include/frameworks/native/include \
+	$(LOCAL_PATH)/include/hardware/libhardware/include \
+	$(LOCAL_PATH)/include/frameworks/av/include \
+
+
+LOCAL_LDLIBS += -L$(LOCAL_PATH)/lib -ljnigraphics -lEGL -lGLESv2 -landroid -landroid_runtime -landroidfw -lbinder -lcutils -lexpat -lgui -lmedia -lnativehelper -lskia -lsqlite -lstlport -lui -lutils -llog -lOpenSLES
+
+
+
 LOCAL_SHARED_LIBRARIES := \
+libEGL \
+             libandroid \
+         libandroidfw \
+       libcutils \
+          libgui \
+             libnativehelper \
+    libsqlite \
+          libui \
+libGLESv2 \
+          libandroid_runtime \
+ libbinder \
+          libexpat \
+           libmedia \
+           libskia \
+            libstlport \
+         libutils \
     libcutils \
     libutils \
     libbinder \
@@ -13,9 +39,12 @@ LOCAL_SHARED_LIBRARIES := \
             libgui \
                 libandroid_runtime \
                     libstagefright_foundation  
-      
+
 LOCAL_MODULE:= libshowYUV  
-  
+
+
 LOCAL_MODULE_TAGS := tests  
-  
+
+
 include $(BUILD_SHARED_LIBRARY)  
+
